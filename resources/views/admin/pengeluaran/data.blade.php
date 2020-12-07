b47c65f3e2bdedf3415f5c155dcd4493935d0751
SmartTamalonggehe <smartspartacus@gmail.com>
2020-12-07 19:10:41 +0900

    perbaiki datatables

--- resources/views/admin/pengeluaran/data.blade.php
+++ resources/views/admin/pengeluaran/data.blade.php
@@ -1,5 +1,5 @@
 @php
-    use Illuminate\Support\Carbon;
+use Illuminate\Support\Carbon;
 @endphp
 <table class="table table-bordered table-hover table-checkable" id="kt_datatable" style="margin-top: 13px !important">
     <thead>
@@ -12,47 +12,43 @@
     </thead>
     <tbody>
         @foreach ($pengeluaran as $item)
-        <tr class="clickable-row" data-id="{{ $item->id }}">
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $item->transaksi->nm_transaksi }}</td>
-            <td>{{ Carbon::parse($item->tgl_kas)->format('d-m-Y') }}</td>
-            <td>@currency($item->pengeluaran)</td>
-        </tr>
+            <tr class="clickable-row" data-id="{{ $item->id }}">
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $item->transaksi->nm_transaksi }}</td>
+                <td>{{ Carbon::parse($item->tgl_kas)->format('d-m-Y') }}</td>
+                <td>@currency($item->pengeluaran)</td>
+            </tr>
         @endforeach
     </tbody>
 </table>
 
 
-<!--begin::Page Vendors(used by this page)-->
-<script src="{{ asset('toolsAdmin/plugins/custom/datatables/datatables.bundle.js?v=7.0.5') }}"></script>
-<!--end::Page Vendors-->
-<!--begin::Page Scripts(used by this page)-->
-<script src="{{ asset('toolsAdmin/js/pages/crud/datatables/data-sources/html.js?v=7.0.5') }}"></script>
-<!--end::Page Scripts-->
+
 
 <script>
     var href;
     $(document).ready(function($) {
         $(".clickable-row").dblclick(function() {
-            href= $(this).data('id');
+            href = $(this).data('id');
             $('#alertPertanyaan').modal('show')
         });
     });
+
 </script>
 
 <script>
-    $('#btnUbah').off('click').on('click',function(e){
+    $('#btnUbah').off('click').on('click', function(e) {
         e.preventDefault()
         $('#alertPertanyaan').modal('hide')
-        save_method="Ubah"
+        save_method = "Ubah"
         $.ajax({
-            url: "pengeluaran/"+href+"/edit", 
+            url: "pengeluaran/" + href + "/edit",
             type: 'GET',
             dataType: 'JSON',
             beforeSend: function() {
                 // lakukan sesuatu sebelum data dikirim
-                console.log(href); 
-                },
+                console.log(href);
+            },
             success: function(data) {
                 // lakukan sesuatu jika data sudah terkirim
                 $('#id').val(data.id);
@@ -64,40 +60,51 @@
                 $('#tombolForm').html('Ubah Data')
             }
         });
-        
+
     });
-    $('#btnHapus').on('click',function(){
+    $('#btnHapus').on('click', function() {
         $('#alertPertanyaan').modal('hide')
-        var csrf_token=$('meta[name="csrf_token"]').attr('content');
+        var csrf_token = $('meta[name="csrf_token"]').attr('content');
         Swal.fire({
-        title: 'Yakin?',
-        text: "Data akan dihapus secara permanen!",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yakin',
-        cancelButtonText: 'Batal',
-        confirmButtonClass: 'btn btn-primary',
-        cancelButtonClass: 'btn btn-danger ml-1',
-        buttonsStyling: false,
-        }).then(function (result) {
-        if (result.value) {
-            $.ajax({
-                url: "pengeluaran/"+href,
-                type : "POST",
-                data : {'_method' : 'DELETE', '_token' :csrf_token},
-                success: function(response) {
-                    Swal.fire({
+            title: 'Yakin?',
+            text: "Data akan dihapus secara permanen!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yakin',
+            cancelButtonText: 'Batal',
+            confirmButtonClass: 'btn btn-primary',
+            cancelButtonClass: 'btn btn-danger ml-1',
+            buttonsStyling: false,
+        }).then(function(result) {
+            if (result.value) {
+                $.ajax({
+                    url: "pengeluaran/" + href,
+                    type: "POST",
+                    data: {
+                        '_method': 'DELETE',
+                        '_token': csrf_token
+                    },
+                    success: function(response) {
+                        Swal.fire({
                             type: "success",
                             title: 'Deleted!',
                             text: 'Your file has been deleted.',
                             confirmButtonClass: 'btn btn-success',
                         })
-                    loadMoreData();
+                        loadMoreData();
                     }
                 })
             }
         });
     });
+
 </script>
+
+<!--begin::Page Vendors(used by this page)-->
+<script src="{{ asset('toolsAdmin/plugins/custom/datatables/datatables.bundle.js?v=7.0.5') }}"></script>
+<!--end::Page Vendors-->
+<!--begin::Page Scripts(used by this page)-->
+<script src="{{ asset('toolsAdmin/js/pages/crud/datatables/data-sources/html.js?v=7.0.5') }}"></script>
+<!--end::Page Scripts-->
